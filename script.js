14bd8981cd054d4e55a02d638518c5ac74400419
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-13 16:18:24 -0400

    decided to change the call to search by title and only show that specific movie

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(event) {
     // stores the user's input
     var searchValue = $("#searchText").val();
     // creates our url used for api call
-    var queryURL = "https://www.omdbapi.com/?s=" + searchValue + "&apikey=db8b059d";
+    var queryURL = "https://www.omdbapi.com/?t=" + searchValue +"&apikey=db8b059d";
     // calls the api
     $.ajax({
       url: queryURL,
@@ -14,25 +14,15 @@ $(document).ready(function(event) {
       // if call is successful run this function
     }).then(function(response) {
       console.log(response);
-      let movies = response.Search; // stores every movie that the search returns
-      let output = ''; // leave it empty so we can add output dynamically
-
-      // jquery each function loops through each item in the movies variable 
-      $.each(movies, function(index, movie) {
-        // output is dynamically added to HTML for each movie
-        output += `
-          <div class="col-md-3>
-            <div class="well text-center">
-              <img src="${movie.Poster}">
-              <h5>${movie.Title}</h5>
-              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-            </div>
-          </div>
-        `;
-      });
-      // output is added as html into the div with ID movies
-      $('#movies').html(output);
-      // if call is unsuccessful run this function
+      let movieTitle = response.Title;
+      let movieYear = response.Year;
+      let movieRuntime = response.Runtime;
+      let movieDirector = response.Director;
+      let movieActors = response.Actors;
+      let moviePoster = response.Poster;
+      let movieScore = response.Ratings[1].Value;
+      console.log(movieTitle);
+      console.log("Score: " + movieScore);
     }).catch(function(error) {
       console.log(error);
     })
