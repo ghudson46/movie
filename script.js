9b84ac00cca48d76826bda902f8eb4fd3ec212cf
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-13 17:13:45 -0400

    saving changes

--- script.js
+++ script.js
@@ -20,9 +20,40 @@ $(document).ready(function(event) {
       let movieDirector = response.Director;
       let movieActors = response.Actors;
       let moviePoster = response.Poster;
+      let moviePlot = response.Plot;
       let movieScore = response.Ratings[1].Value;
-      console.log(movieTitle);
-      console.log("Score: " + movieScore);
+      let movieContainer = document.getElementById("movies");
+      let linebreak = document.createElement("br");
+      
+      movieContainer.innerHTML += "<img src='" + moviePoster + "'>";
+      movieContainer.appendChild(linebreak);
+      movieContainer.innerHTML += "<h5>" + movieTitle + "</h5>";
+      movieContainer.appendChild(linebreak);
+      movieContainer.innerHTML += "<h5>" + movieYear + "</h5>";
+      movieContainer.appendChild(linebreak);
+      movieContainer.innerHTML += "<h5>" + movieRuntime + "</h5>";
+      movieContainer.appendChild(linebreak);
+      movieContainer.innerHTML += "<h5>" + movieDirector + "</h5>";
+      movieContainer.appendChild(linebreak);
+      movieContainer.innerHTML += "<h5>" + movieActors + "</h5>";
+      movieContainer.appendChild(linebreak);
+      movieContainer.innerHTML += "<h5>" + moviePlot + "</h5>";
+      movieContainer.appendChild(linebreak);
+      movieContainer.innerHTML += "<h5 id='score'>Score: " + movieScore + "</h5>";
+
+      console.log(movieScore);
+        
+      let score = document.getElementById('score');
+      if (movieScore == '100%' || movieScore == '99%' || movieScore == '98%' || movieScore == '97%' || movieScore == '96%' || movieScore == '95%' || movieScore == '94%' || movieScore == '93%' || movieScore == '92%' || movieScore == '91%' || movieScore == '90%') {
+        score.classList.add("green");
+      } else if (movieScore == '89%' || movieScore == '88%' || movieScore == '87%' || movieScore == '86%' || movieScore == '85%' || movieScore == '84%' || movieScore == '83%' || movieScore == '82%' || movieScore == '81%' || movieScore == '80%') {
+        score.classList.add("yellow");
+      } else if (movieScore == '79%' || movieScore == '78%' || movieScore == '77%' || movieScore == '76%' || movieScore == '75%' || movieScore == '74%' || movieScore == '73%' || movieScore == '72%' || movieScore == '71%' || movieScore == '70%') {
+        score.classList.add("orange");
+      } else {
+        score.classList.add("red");
+      }
+
     }).catch(function(error) {
       console.log(error);
     })
