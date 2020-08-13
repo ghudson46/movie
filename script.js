658aa568ad3561865e1d5e245713c68111b55138
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-13 00:43:19 -0400

    created the output formatting for the HTML

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ $(document).ready(function(event) {
   $("#searchForm").on("submit", function(event) {
     event.preventDefault();
     // stores the user's input
-    var movie = $("#searchText").val();
+    var searchValue = $("#searchText").val();
     // creates our url used for api call
-    var queryURL = "https://www.omdbapi.com/?s=" + movie + "&apikey=db8b059d";
+    var queryURL = "https://www.omdbapi.com/?s=" + searchValue + "&apikey=db8b059d";
     // calls the api
     $.ajax({
       url: queryURL,
@@ -14,6 +14,19 @@ $(document).ready(function(event) {
       // if call is successful run this function
     }).then(function(response) {
       console.log(response);
+      let movies = response.Search;
+      let output = '';
+      $.each(movies, function(index, movie) {
+        output += `
+          <div class="col-md-3>
+            <div class="well text-center">
+              <img src="${}movie.Poster">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `;
+      })
       // if call is unsuccessful run this function
     }).catch(function(error) {
       console.log(error);
